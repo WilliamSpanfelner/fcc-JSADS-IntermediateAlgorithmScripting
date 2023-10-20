0fb50c047ac05a89c8beda76a5fe1d9770998262
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-20 12:50:59 +0300

    Challenge - solution

--- 03_seek&Destroy.js
+++ 03_seek&Destroy.js
@@ -10,8 +10,14 @@ also known as a variadic function. You can access the additional
 arguments by adding a rest parameter to the function definition 
 or using the arguments object.
 */
-function destroyer(arr) {
-    return arr;
+function destroyer(arr, ...args) {
+    return arr.filter(element => !args.includes(element));
 }
-
-destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+const testData = [
+    [["tree", "hamburger", 53], "tree", 53],
+    [[2, 3, 2, 3], 2, 3],
+    [[3, 5, 1, 2, 2], 2, 3, 5],
+    [[1, 2, 3, 5, 1, 2, 3], 2, 3],
+    [[1, 2, 3, 1, 2, 3], 2, 3]
+];
+console.log(destroyer(testData[0][0], testData[0][1], testData[0][2], testData[0][3]));
