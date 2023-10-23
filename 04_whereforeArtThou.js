fa3aeb4fc979739812d60c40a49653e20471463d
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-23 20:10:47 +0300

    Filter elements for one element  source input.

--- 04_whereforeArtThou.js
+++ 04_whereforeArtThou.js
@@ -16,12 +16,39 @@ it contains the name and its value, that was passed on as
 the second argument.
 */
 function whatIsInAName(collection, source) {
+    // Get the objects from the source argument and then their values
+    const sourceEntries = Object.entries(source);
+    const sourceKey = sourceEntries[0][0]; // Object.keys(source);
+    const sourceValue = sourceEntries[0][1] // source[sourceKey];
 
+    // Test each element of the collection to find matches
+    const newArr = collection.map((element) =>
+    element.hasOwnProperty(sourceKey) && element[sourceKey] == sourceValue);
+    console.log(newArr);
+
+    // Create an array of results using a for loop
+    const newerArr = [];
+    for (let i = 0; i < collection.length; i++) {
+        const element = collection[i];
+        if (element.hasOwnProperty(sourceKey) && element[sourceKey] == sourceValue) {
+            newerArr.push(element);
+        }
+    }
+    console.log(newerArr);
+
+    // Create the same array as above with the filter method.
+    const newestArr = collection.filter((element) =>
+    element.hasOwnProperty(sourceKey) && element[sourceKey] == sourceValue);
+    console.log(newestArr);
+
+    return newestArr;
 }
 
-whatIsInAName([
-    { first: "Romeo", last: "Montague" },
-    { first: "Mercutio", last: null }, 
-    { first: "Tybalt", last: "Capulet" }], 
-    { last: "Capulet" }
-    );
+console.log(whatIsInAName([{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }], { "apple": 1 }));
+
+// console.log(whatIsInAName([
+//     { first: "Romeo", last: "Montague" },
+//     { first: "Mercutio", last: null }, 
+//     { first: "Tybalt", last: "Capulet" }], 
+//     { last: "Capulet" }
+//     ));
