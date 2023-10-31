6f599d15b1380c71dc37a485da177c9b6a7bcf6b
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-10-31 16:41:41 +0200

    Replace spaces with hyphens

--- 05_spinalTapCase.js
+++ 05_spinalTapCase.js
@@ -5,8 +5,12 @@ all-lowercase-words-joined-by-dashes. */
 
 function spinalCase(str) {
     // convert string to lowercase
-    const lowercase = str.toLowerCase();
-    console.log(lowercase);
+    const lowercaseStr = str.toLowerCase();
+    console.log(lowercaseStr);
+
+    // replace spaces with hyphens
+    const spinalcase = lowercaseStr.replace(/\s/g, '-');
+    console.log(spinalcase);
     return str;
 }
 
