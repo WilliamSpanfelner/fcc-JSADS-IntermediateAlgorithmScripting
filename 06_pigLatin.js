9cec70cdb8084e14ada194631abee81923275f8c
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-11-04 19:58:14 +0200

    Refactor to use more functional approach

--- 06_pigLatin.js
+++ 06_pigLatin.js
@@ -15,36 +15,61 @@ guaranteed to be English words in all lowercase.
 
 function translatePigLatin(str) {
     const regex = /^[aeiou]/;
-    const vowelPrefix = str.match(regex) ? str.concat('way') : str;
-    console.log(vowelPrefix);
-    const vowels = ['a', 'e', 'i', 'o', 'u'];
     
-    const charArray = str.split('');
-    let maxIndex;
+    function translateConsonantPrefix(string) {
+        const str2Translate = string.split('');
+        const indexOfLastConsonant = str2Translate.indexOf(
+            [str2Translate.find(element => element.match(regex))]
+            .filter(element => element !== undefined)[0]);
+
+        if (indexOfLastConsonant > -1) {        
+            const consonantChunk = str2Translate.slice(0, indexOfLastConsonant);
+            const bodyChunk = str2Translate.slice(indexOfLastConsonant);
+            return bodyChunk.concat(consonantChunk).join('') + 'ay';
+        } else {
+            return str + 'ay'
+        } 
+    }
+    
+    const translation = str.match(regex) ? 
+    str.concat('way') : 
+    translateConsonantPrefix(str);
+
+    // console.log(translation);
+
+
+    // const vowels = ['a', 'e', 'i', 'o', 'u'];
+    
+    // const charArray = str.split('');
+    // let maxIndex;
     
     // iterate charArray until a vowel is found logging 
     // the maxIndex of the last prefix consonant
-    for (let i = 0; i < charArray.length; i++) {
-        const element = charArray[i];
+    // for (let i = 0; i < charArray.length; i++) {
+    //     const element = charArray[i];
         
-        if (vowels.includes(element)) {
-            break;
-        } else {
-            maxIndex = charArray.indexOf(element);
-            // console.log(element, maxIndex, charArray);
-        }
-    }
+    //     if (vowels.includes(element)) {
+    //         break;
+    //     } else {
+    //         maxIndex = charArray.indexOf(element);
+    //         // console.log(element, maxIndex, charArray);
+    //     }
+    // }
 
     // Get the consonant prefix of the word
-    if (maxIndex != undefined) {
-        const consonantChunk = charArray.slice(0, maxIndex + 1);
-        const bodyChunk = charArray.slice(maxIndex + 1);
-        const newWord = bodyChunk.concat(consonantChunk).join('') + 'ay';
-        // console.log(consonantChunk, bodyChunk, newWord);
-        return newWord;
-    } else {  // word must begin with a vowel
+    // if (maxIndex != undefined) {
+    //     const consonantChunk = charArray.slice(0, maxIndex + 1);
+    //     const bodyChunk = charArray.slice(maxIndex + 1);
+    //     const newWord = bodyChunk.concat(consonantChunk).join('') + 'ay';
+    //     // console.log(consonantChunk, bodyChunk, newWord);
+    //     return newWord;
+    // } 
+    
+    // else {  // word must begin with a vowel
         // return str + 'way'
-    }
+    // }
+
+    return translation;
 }
 
 const testData = [
