9980fe2dbfbdff88a42d46bd27df55dc05824ecc
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-11-08 21:31:25 +0200

    Refactor using an object and map elements to the object properties.

--- 08_dnaPairing.js
+++ 08_dnaPairing.js
@@ -16,23 +16,31 @@ The character and its pair are paired up in an array, and all the
 arrays are grouped into one encapsulating array.
 */
 function pairElement(str) {
-    let dnaPairs = [];
+    const pairs = {
+        A: "T",
+        T: "A",
+        C: "G",
+        G: "C"
+    };
+    // let dnaPairs = [];
     const unpairedElements = str.split('');
     
-    unpairedElements.forEach(element => {
-        let pairedElement;
-        if (element == "G") {
-            pairedElement = [element, "C"];
-        } else if (element == "C") {
-            pairedElement  = [element, "G"];
-        } else if (element == "A") {
-            pairedElement  = [element, "T"];
-        } else if (element == "T") {
-            pairedElement  = [element, "A"];
-        }
-        dnaPairs.push(pairedElement);
-    });
+    // unpairedElements.forEach(element => {
+    //     let pairedElement;
+    //     if (element == "G") {
+    //         pairedElement = [element, "C"];
+    //     } else if (element == "C") {
+    //         pairedElement  = [element, "G"];
+    //     } else if (element == "A") {
+    //         pairedElement  = [element, "T"];
+    //     } else if (element == "T") {
+    //         pairedElement  = [element, "A"];
+    //     }
+    //     dnaPairs.push(pairedElement);
+    // });
 
+    const dnaPairs = unpairedElements.map(element => [element, pairs[element]]);
+    
     return dnaPairs;
 }
 
