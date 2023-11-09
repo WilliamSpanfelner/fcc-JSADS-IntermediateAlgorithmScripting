3c391bf62dffe37459585d9be578f7038408a7f6
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-11-09 18:19:55 +0200

    Get UTF-16 codes for start and end chars of str

--- 09_missingLetters.js
+++ 09_missingLetters.js
@@ -8,6 +8,15 @@ If all letters are present in the range, return undefined.
 */
 
 function fearNotLetter(str) {
+    // Get the UTF-16 code for starting and ending chars
+    const startRange = str.charCodeAt(0);
+    const endRange = str.charCodeAt(str.length - 1);
+    // console.log(startRange.charCodeAt(0));
+    // console.log(endRange.charCodeAt(0));
+    // console.log(endRange.charCodeAt(0) - startRange.charCodeAt(0) + 1);
+    // console.log(str.length);
+    console.log(str.charCodeAt(0), str.charCodeAt(str.length - 1));
+
     return str;
 }
 
