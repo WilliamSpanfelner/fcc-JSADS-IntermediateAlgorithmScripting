67749ae89f08664ff0400ade53e1239950887106
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-11-11 15:41:50 +0200

    Challenge - start

---
+++ 11_convertHTMLEntities.js
@@ -0,0 +1,14 @@
+// Convert HTML Entities
+
+/*
+Convert the characters &, <, >, " (double quote), 
+and ' (apostrophe), in a string to their corresponding 
+HTML entities.
+*/
+
+function convertHTML(str) {
+    return str;
+}
+
+convertHTML("Dolce & Gabbana");
+
