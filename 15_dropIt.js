96abce7191e816a6c7599b3875f70f2c841123ad
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-11-19 20:26:15 +0200

    Add test data Challenge - complete

--- 15_dropIt.js
+++ 15_dropIt.js
@@ -24,4 +24,9 @@ function dropElements(arr, func) {
     // Otherwise return an empty array
     return [];
 }
-console.log(dropElements([1, 2, 3], function(n) {return n < 3; }));          // [1, 2, 3] [1, 2]
+console.log(dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;}));  // [3, 9, 2] [3, 9]
+// console.log(dropElements([1, 2, 3, 7, 4], function(n) {return n > 3;}));  // [7, 4]    [7, 4]
+// console.log(dropElements([1, 2, 3, 4], function(n) {return n > 5;}));     // []        []
+// console.log(dropElements([1, 2, 3], function(n) {return n > 0;}));        // [1, 2, 3] [1, 2, 3]
+// console.log(dropElements([0, 1, 0, 1], function(n) {return n === 1;}));   // [1, 0, 1] [1, 1]
+// console.log(dropElements([1, 2, 3], function(n) {return n < 3; }));          // [1, 2, 3] [1, 2]
