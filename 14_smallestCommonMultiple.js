c2a9dd8c1fd08e2d5315ad1af17d75e3ca06b5db
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-11-19 16:58:00 +0200

    Remove allElementsEqual function;
    Refactor converging definition;
    Create lcmFactors and primes arrays;
    Create a primeIndex counter;
    Create a foundFactor boolean flag;
    Create testData;

--- 14_smallestCommonMultiple.js
+++ 14_smallestCommonMultiple.js
@@ -16,17 +16,18 @@ here would be 6.
 */
 
 function smallestCommons(arr) {
-    /**
-     * Compare elements for equality
-     * @param {*} arr 
-     * @returns bool
-     */
-        function allElementsEqual(arr) {
-            const element0 = arr[0]
-            return arr.reduce((result, element) => element == element0, false);
+        /**
+         * Check array to see if all elements == 1
+         * @param {*} arr 
+         */
+        function elementsNotOne(arr) {
+            const equalsOne = arr.reduce((results, element) => {
+                results.push(element == 1);
+                return results;
+            }, []);
+            return equalsOne.includes(false);
+            // return arr.map(element => element == 1).includes(false);
         }
-
-
     // Determine min and max values of arr
     const min_arr_Value = Math.min(...arr);
     const max_arr_Value = Math.max(...arr);
@@ -36,19 +37,34 @@ function smallestCommons(arr) {
     for (let index = min_arr_Value; index < max_arr_Value + 1; index++) {
         rangeValues.push(index);
     }
-    console.log(rangeValues);
 
-    // Multiply first element by 2 and see if all elements are equivalent
-    let converging = rangeValues.concat([]).filter(element => element > 1);
-    converging[0] *= 2
-    console.log(converging);
+    // Filter rangeValues elements > 1
+    const initialValues = rangeValues.filter(element => element > 1);
 
-    // Are all elements equivalent now?
-    // if (allElementsEqual(converging)) {
-        console.log(allElementsEqual(converging));
-    // }
-    return arr  // leastCommonMultiple;
+    // Create a copy of initialValues for mutating
+    let converging = initialValues.concat([]);
+
+
+    let lcmFactors = [];
+    const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23];
+    let primeIndex = 0;
+    let foundFactor = false;
+
+    return arr;
 }
 
-smallestCommons([1,5]);
+const testData = [
+    [3, 1],  // 6
+    [5, 1],  // 60
+    [2, 10],  // 2520
+    [1, 13],  // 360360
+    [23, 18]  // 6056820
+];
+
+testData.forEach(element => 
+    console.log("LCM is:", smallestCommons(element))
+);
+
+
+// console.log("LCM is:", smallestCommons([5, 1]));
 
