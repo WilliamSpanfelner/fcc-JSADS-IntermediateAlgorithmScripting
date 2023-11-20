ca4a013b3353e2536486b1df7690e212c3465219
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-11-20 20:25:02 +0200

    Create arrDepths array with arr element lengths

--- 16_steamroller.js
+++ 16_steamroller.js
@@ -6,24 +6,32 @@ You must account for varying levels of nesting.
 */ 
 
 function steamrollArray(arr) {
-    // let depth = 0;
+    // Get the lengths of all elements in arr
+    const arrDepths = arr.reduce((depth, element) => {
+        depth.push(element.length);
+        return depth;
+    }, []);
+    console.log(arrDepths);  // [undefined, undefined, 2]
     // for (const i of arr) {
     //     if (i.length > depth) {
     //         depth = i.length;
     //     }    
     // }
+    return arr;
+    // Solution using flatMap and flat not permitted
+
     // find the greatest depth of nesting
-    const depth = arr.reduce((depth, element) => {
-        if (element.length > depth) {
-            depth = element.length
-        }
-        return depth;
-    }, 0);
-    console.log(depth);
+    // const depth = arr.reduce((depth, element) => {
+    //     if (element.length > depth) {
+    //         depth = element.length
+    //     }
+    //     return depth;
+    // }, 0);
+    // console.log(depth);
     /* use flatMap to return a flattened array for all but 
     the deepest nesting and then the flat method to perform 
     final flattening */
-    return arr.flatMap(element => element).flat(depth);
+    // return arr.flatMap(element => element).flat(depth);
 }
 
 
