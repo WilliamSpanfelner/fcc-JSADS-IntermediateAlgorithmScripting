ace8a5fa3e48d44f35fd8869fc80f71f09d6c58d
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-11-20 17:49:11 +0200

    Challenge - start

---
+++ 16_steamroller.js
@@ -0,0 +1,12 @@
+// Steamroller
+
+/*
+Flatten a nested array. 
+You must account for varying levels of nesting.
+*/ 
+
+function steamrollArray(arr) {
+    return arr;
+}
+
+console.log(steamrollArray([1, [2], [3, [[4]]]]));
